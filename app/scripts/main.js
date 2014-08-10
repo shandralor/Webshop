5c71661fb4bea0acfbbf57e15b21c2c49176b714
Bryan <p.b.caudron@gmail.com>
2014-08-10 13:04:47 +0200

    test commit

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global Webshop, $*/
+
+
+window.Webshop = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    Webshop.init();
+});
